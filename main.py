81f981c1f2971a628bd33742b6d8e2c4cf201ed3
zhekas <zhekas2003@mail.ru>
2024-03-12 19:52:25 +0300

    Revert "Добавил открытие нового бота + обращение по имени"
    This reverts commit 0bd9bee0357f8d24bc69026f724e43591780f1fd.

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 import telebot
-import webbrowser
+
 Mafia = telebot.TeleBot('6847605581:AAFshf6F811PHzdMu98gvEg26RRNOrLf2Y8')
 
 
 @Mafia.message_handler(commands=['start'])
 def main(message):
     Mafia.send_message(message.chat.id, 'Привет я всего лишь бот, который поможет вам начать игру.При помощи команд вы сможеле:/help - узнать информацию о командах, /play - узнать правило игры, /rules - узнать правило игры!')
-
-@Mafia.message_handler(commands=["open_new_bot"])
-def new_bot_open(message):
-    webbrowser.open("https://t.me/Punching_phone_bot")
-
-
-@Mafia.message_handler(commands=["get_my_name"])# Команда, которая демонстрирует возможность обращения к пользоваелю по имени
-def info(message):
-    Mafia.send_message(message.chat.id, f'Привет: {message.from_user.first_name} {message.from_user.last_name}')
 
 @Mafia.message_handler(func=lambda message: message.text.lower() == '/help')
 def idd(message):
@@ -25,6 +16,7 @@ def idd(message):
     Mafia.send_message(message.chat.id, 'Правило игры!!!Мирные жители узнают что в их городе есть мафия, и они пытаються методом голосования посадить их в тюрму.Мафия же пытаются убить всех мирных жителей.У мрных жителей есть доктор и коммисар. Доктор пытается вылечит всех мирных жителей для их спасения, а коммисар пытается вычеслить мафию для помощи жителям в голосовании. Сасмое главное в этой игре коммуникация (умение лгать или же доказывать правду). Желаю вам удачи и приятной игры!!!') # команда выводит текст с правилом игры
 
 
+ff
 @Mafia.message_handler()
 def info(message):
     Mafia.send_message(message.chat.id, 'Я всего лишь бот, воспользуйтесь командой /help') # при написании любого текста бот выводит данный текст
