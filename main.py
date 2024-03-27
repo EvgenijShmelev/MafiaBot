05bfbae3a7e7efdc582322b0460e125cad4618a7
zhekas <zhekas2003@mail.ru>
2024-03-27 16:45:41 +0300

    NewBot

--- main.py
+++ main.py
@@ -1,25 +1,66 @@
 import telebot
+from telebot import types
+from roles import MafiaPlayer
+import random
 
 Mafia = telebot.TeleBot('6847605581:AAFshf6F811PHzdMu98gvEg26RRNOrLf2Y8')
 
+users = set()
 
-@Mafia.message_handler(commands=['start'])
-def main(message):
-    Mafia.send_message(message.chat.id, 'Привет я всего лишь бот, который поможет вам начать игру.При помощи команд вы сможеле:/help - узнать информацию о командах, /play - узнать правило игры, /rules - узнать правило игры!')
+Name = ["Bob", "Alpha", "Sugar", "Pahan", "Noob", "Thief", "Tim", "Logan"]
 
-@Mafia.message_handler(func=lambda message: message.text.lower() == '/help')
-def idd(message):
-    Mafia.send_message(message.chat.id, 'Присутствуют такие команды как: /rules (Правило игры), /play (присоедениться к игре)') #команда выводит команды для использования бота
+Role = ["Mafia", "Mafia", "cityzen", "cityzen", "cityzen", "cityzen", "Doctor", "Sheriff"]
 
-@Mafia.message_handler(func=lambda message: message.text.lower() == '/rules')
-def idd(message):
-    Mafia.send_message(message.chat.id, 'Правило игры!!!Мирные жители узнают что в их городе есть мафия, и они пытаються методом голосования посадить их в тюрму.Мафия же пытаются убить всех мирных жителей.У мрных жителей есть доктор и коммисар. Доктор пытается вылечит всех мирных жителей для их спасения, а коммисар пытается вычеслить мафию для помощи жителям в голосовании. Сасмое главное в этой игре коммуникация (умение лгать или же доказывать правду). Желаю вам удачи и приятной игры!!!') # команда выводит текст с правилом игры
+waiting_room = []
+
+lobby = []
+
+@Mafia.message_handler(commands=['find'])
+def finde_thegame(message):
+
+    waiting_room.append(message.from_user.id)
+
+    
+    Mafia.send_message(message.from_user.id, f"Вы попали в комнату ожидания. В ней {len(waiting_room)} игрок(а)") 
+
+    for user in waiting_room:
+        if user != message.from_user.id:
+            Mafia.send_message(user, f'Количество игроков в комнате ожидания = {len (waiting_room)}')  # Сообщение всем остальным игрокам о появление нового игрока
+            
+    if len(waiting_room) == 2:
+        random_name = Name  
+        random.shuffle(random_name)  # перебор массива имён
+        random_role = Role 
+        random.shuffle(random_role)   # перебор массива ролей
+
+        for i in range(0, len(waiting_room)):
+            player = MafiaPlayer()
+            player.id_chat = waiting_room[i]
+            player.name = random_name[i]
+            player.role = random_role[i]
+            lobby.append(player)
 
 
-ff
-@Mafia.message_handler()
-def info(message):
-    Mafia.send_message(message.chat.id, 'Я всего лишь бот, воспользуйтесь командой /help') # при написании любого текста бот выводит данный текст
+        for i in range(0,len(lobby)):
+            player = lobby[i]
+            Mafia.send_message(player.id_chat, f"Ваше имя {player.name}, ваша роль {player.role}") 
 
 
-Mafia.polling(none_stop=True) # не даёт выключать компиляцию
+@Mafia.message_handler(func=lambda message: True)
+def on_message(message):
+    print(message)
+
+    for user in users:
+        if user != message.from_user.id:
+            Mafia.send_message(user, message.text)   # общий анонимный чат
+
+    users.add(message.from_user.id)
+
+Mafia.polling(none_stop=True)   # точка входа
+
+
+
+
+
+
+
---
+++ roles.py
@@ -0,0 +1,7 @@
+class MafiaPlayer():
+    def __init__(self):
+        self.name = "Bob"
+        self.role = "cityzen"
+        self.id_chat = 0
+        alive = True
+        
